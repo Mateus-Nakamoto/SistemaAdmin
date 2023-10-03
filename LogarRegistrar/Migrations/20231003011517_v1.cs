686f8d79ecf09340fee66c093dc4c4a0043af234
MATEUSNAKAMOTO <DRAKAMZEN@GMAIL.COM>
2023-10-03 01:02:20 -0300

    Adicionar arquivos de projeto.

---
+++ LogarRegistrar/Migrations/20231003011517_v1.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LogarRegistrar.Migrations
+{
+    /// <inheritdoc />
+    public partial class v1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    pessoaId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    pessoaNome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    pessoaEmail = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    pessoaUsuario = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    pessoaSenha = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Token = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Role = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    pessoaTipo = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.pessoaId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
---
+++ LogarRegistrar/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LogarRegistrar.Models
+{
+    public class User
+    {
+        [Key]
+        public int pessoaId { get; set; }
+        public string pessoaNome { get; set; }
+        public string pessoaEmail { get; set; }
+        public string pessoaUsuario { get; set; }
+        public string pessoaSenha { get; set; }
+        public string Token { get; set; }
+        public string Role { get; set; }
+        public bool pessoaTipo {  get; set; }
+
+    }
+}
